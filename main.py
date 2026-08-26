2928b6978402a71aa15a9ade7b6a4f4acf2c88a6
Edwina Faustin <edwinafaustin15@gmail.com>
2026-08-26 10:43:54 -0400

    add main.py via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#####################################
+# Edwina Faustin # efaustin@usf.edu #
+#___________________________________#
+# main.py: prints "Hello, World!" and #
+# examines behavior of data types.  #
+#####################################
+
+""" we want this script to PRINT  the string" Hello, World!
+"""
+print("Hello, World!")
+
+a = "Hello, World!"
+print(a)
+
